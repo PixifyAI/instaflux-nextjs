de4c0fa0b66d3cc6942d781e0484e8d0acd4d571
Pixify AI <162300760+PixifyAI@users.noreply.github.com>
2024-09-10 23:04:09 -0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import RunwareApp from '@/components/RunwareApp'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-900">
+      <RunwareApp />
+    </main>
+  )
+}
