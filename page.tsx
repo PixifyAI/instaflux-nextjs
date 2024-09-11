84c3a6f9ed1a2861b1cbf6eb48176d5a702a389b
Pixify AI <162300760+PixifyAI@users.noreply.github.com>
2024-09-11 03:31:11 -0500

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-import RunwareApp from '@/components/RunwareApp'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-900">
-      <RunwareApp />
-    </main>
-  )
-}
